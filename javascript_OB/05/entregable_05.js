3bc28ffaf619cd1f2bf2af4512899a4195a04151
ignacioccugura <ignaciocruzcugura@gmail.com>
2023-03-27 20:34:43 -0300

    06 arrays js

---
+++ javascript_OB/05/entregable_05.js
@@ -0,0 +1,10 @@
+let alturaCM = 180;
+let alturaMT = 1.80;
+let peso = 100.05;
+let alturaR = alturaMT.toPrecision(1);
+let pesoR = peso.toPrecision(3);
+
+console.log('alturaR :>> ', alturaR);
+console.log('pesoR :>> ', pesoR);
+let x = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
+console.log('x :>> ', x);
